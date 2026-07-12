997e2d9785a7c2d56127203da8d9fffc3f3fdaac
NguyenHuynhPhuc <phucnh.ce190359@gmail.com>
2026-07-12 15:08:58 +0700

    fix bug

--- apps/nest-ai-service/src/main.ts
+++ apps/nest-ai-service/src/main.ts
@@ -45,7 +45,7 @@ async function bootstrap() {
 
   await app.startAllMicroservices();
   
-  const port = process.env.PORT || 3001;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
 
   console.log(`Nest AI Service is running on HTTP port ${port}`);
